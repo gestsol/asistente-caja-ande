1bbf7040ef3501706e67dd07180baa8d143fe22b
EdixonAlberto <edixonalbertto@gmail.com>
2022-02-03 22:31:14 +0000

    create structure proyect

--- src/utils/config.util.ts
+++ src/utils/config.util.ts
@@ -1,14 +1,12 @@
 export async function loadConfig(): Promise<void> {
   const { resolve } = await import('path')
   const { config } = await import('dotenv')
+
   const NODE_ENV = process.env.NODE_ENV as TEnv
+  const result = config({ path: resolve('env', `${NODE_ENV}.env`) })
 
-  if (NODE_ENV !== 'production') {
-    const result = config({ path: resolve('env', `${NODE_ENV}.env`) })
-
-    if (result.error) throw new Error(`!! ERROR-ENV: ${result.error.message}`)
-    else console.log(`>> ENV: environment "${NODE_ENV}" loaded successfully`)
-  }
+  if (result.error) throw new Error(`ERROR-ENV: ${result.error.message}`)
+  else console.log(`ENV: environment "${NODE_ENV}" loaded successfully`)
 
   const ENV: NodeJS.ProcessEnv = process.env
 
