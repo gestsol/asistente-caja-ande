07044827b0c8891e854bd1b49fd80b2a41cf2423
EdixonAlberto <edixonalbertto@gmail.com>
2022-03-16 19:34:08 +0000

    add new filter in midd requestDebug

--- src/middlewares/requestDebug.mid.ts
+++ src/middlewares/requestDebug.mid.ts
@@ -2,7 +2,22 @@ import { Request, NextFunction } from 'express'
 import { botDebug } from '~UTILS/debug.util'
 
 export function requestDebug(req: Request, _, next: NextFunction): void {
-  botDebug('HTTP-IN', `${req.method} ${req.originalUrl}`)
+  const isStartApi = req.originalUrl.startsWith('/api')
+
+  if (isStartApi) {
+    botDebug('HTTP-IN', `${req.method} ${req.originalUrl}`)
+  }
+
+  if (req.method === 'POST') {
+    const {
+      data: { fromNumber },
+      device: { phone }
+    } = req.body as TWassiRequest
+
+    if (fromNumber !== phone) {
+      botDebug('HTTP-IN', `${req.method} ${req.originalUrl}`)
+    }
+  }
 
   next()
 }
