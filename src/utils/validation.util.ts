c348db6263e90bac8934d81858d8f0c2ec5fe8d4
EdixonAlberto <edixonalbertto@gmail.com>
2022-03-21 13:57:36 +0000

    modify validation email

--- src/utils/validation.util.ts
+++ src/utils/validation.util.ts
@@ -1,3 +1,3 @@
 export const isNumber = (message: string): number | null => (!isNaN(Number(message)) ? Number(message) : null)
 
-export const isEmail = (message: string): boolean => new RegExp(/^[a-z0-9_.]+\@[a-z]+\.[a-z]+$/).test(message)
+export const isEmail = (message: string): boolean => new RegExp(/^[a-z0-9_.]+\@[a-z.]+$/).test(message)
